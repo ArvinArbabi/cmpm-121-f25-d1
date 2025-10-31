ffe4dc0c8f9852caab2006dab9ef75770db14e8b
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-10-31 17:26:42 +0000

    attempt 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+// vite.config.js
 // https://vitejs.dev/config/
 export default {
-  base: Deno.env.get("cmpm-121-f25-d1") || "/project",
+  // Correct base path for your repo
+  base: "https://arvinarbabi.github.io/cmpm-121-f25-d1/",
+
   server: {
     port: 3000,
     open: true,
   },
+
   build: {
     target: "esnext",
     outDir: "dist",
