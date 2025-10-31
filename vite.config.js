3ad5bac88972ff0c0dfaa6991f7ec73242c87f53
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-10-31 17:32:14 +0000

    why aint this working what

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-// vite.config.js
 // https://vitejs.dev/config/
 export default {
-  // Correct base path for your repo
-  base: "https://arvinarbabi.github.io/cmpm-121-f25-d1/",
-
+  base: "/cmpm-121-f25-d1/",
   server: {
     port: 3000,
     open: true,
   },
-
   build: {
     target: "esnext",
     outDir: "dist",
