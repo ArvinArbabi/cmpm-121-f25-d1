22bcbcc3bac3f82d6c54ac2bda485a0190115554
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-10-31 22:42:41 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 export default {
   // must match https://arvinarbabi.github.io/**cmpm-121-f25-d1**/
-  // WHY WONT THIS WORK!!!!
+  // WHY WONT THIS WORK!!!!!
   base: "/cmpm-121-f25-d1/",
   server: { port: 3000, open: true },
   build: { target: "esnext", outDir: "dist", sourcemap: true },
