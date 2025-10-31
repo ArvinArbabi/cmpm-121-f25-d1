ede45ccd2a88525329797afb3710d37d7359301d
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-10-31 17:35:08 +0000

    attempt 4

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
+// vite.config.js
 // https://vitejs.dev/config/
 export default {
-  base: "/cmpm-121-f25-d1/",
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    sourcemap: true,
-  },
+  base: Deno.env.get("REPO_NAME")
+    ? `/${Deno.env.get("REPO_NAME")}/`
+    : "/",
+  server: { port: 3000, open: true },
+  build: { target: "esnext", outDir: "dist", sourcemap: true },
 };
+
