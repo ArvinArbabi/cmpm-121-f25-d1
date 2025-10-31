d5e337f53728912d409a2aaf5581eae93b5da76a
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-10-31 22:26:41 +0000

    please work

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-// https://vitejs.dev/config/
+// vite.config.js
 export default {
-  base: Deno.env.get("REPO_NAME") ? `/${Deno.env.get("REPO_NAME")}/` : "/",
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    sourcemap: true,
-  },
+  // must match https://arvinarbabi.github.io/**cmpm-121-f25-d1**/
+  // WHY WONT THIS WORK!!!!
+  base: "/cmpm-121-f25-d1/",
+  server: { port: 3000, open: true },
+  build: { target: "esnext", outDir: "dist", sourcemap: true },
 };
